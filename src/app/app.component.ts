c09f261c3ef3b6ceeb6d1e9e3b75ed7aea988e04
Daniele Ferrari <danieleferrari.gt4w@gmail.com>
2019-02-24 19:39:50 -0300

    criando arquivo em portugues e adicionando como default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ import {TranslateService} from '@ngx-translate/core';
 export class AppComponent {
   title = 'portfolio';
   constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en');
+    translate.setDefaultLang('pt');
   }
 }
